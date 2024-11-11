c62dc3661864e3e4107ffff51e17aba519ad6924
fummbly <tstone449@gmail.com>
2024-11-11 09:38:06 -0500

    Created readiness handler

--- main.go
+++ main.go
@@ -5,11 +5,20 @@ import (
 	"net/http"
 )
 
+func handlerReadiness(w http.ResponseWriter, req *http.Request) {
+
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+
+}
+
 func main() {
 	port := "8080"
 	directory := "."
 	servMux := http.NewServeMux()
-	servMux.Handle("/", http.FileServer(http.Dir(directory)))
+	servMux.HandleFunc("/healthz", handlerReadiness)
+	servMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(directory))))
 	server := http.Server{
 		Handler: servMux,
 		Addr:    ":" + port,
