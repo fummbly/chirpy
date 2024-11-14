497ee25e1957933d331e648e956f2b68d7df79d4
fummbly <tstone449@gmail.com>
2024-11-14 13:39:49 -0500

    added a function for censoring chirps

--- validate.go
+++ validate.go
@@ -3,37 +3,53 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"slices"
+	"strings"
 )
 
+func removeProfainity(str string) string {
 
+	profainity := []string{"kerfuffle", "sharbert", "fornax"}
 
+	words := strings.Split(str, " ")
+	for i, word := range words {
+		if slices.Contains(profainity, strings.ToLower(word)) {
+			words[i] = "****"
+		}
+	}
+
+	newStr := strings.Join(words, " ")
+
+	return newStr
+
+}
 
 func handleValidate(w http.ResponseWriter, req *http.Request) {
 
-  type parameters struct {
-    Body string `json:"body"`
-  }
-  type returnVal struct {
-    Valid bool `json:"valid"`
-  }
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	type returnVal struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
 
-  decoder := json.NewDecoder(req.Body)
-  params := parameters{}
-  err := decoder.Decode(&params)
-  if err != nil {
-    respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
-    return
-  }
+	decoder := json.NewDecoder(req.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
+		return
+	}
 
-  const chripLength = 140
+	const chripLength = 140
 
-  if len(params.Body) > chripLength {
-    respondWithError(w, http.StatusBadRequest, "Chirp too long", nil)
-    return
-  }
+	if len(params.Body) > chripLength {
+		respondWithError(w, http.StatusBadRequest, "Chirp too long", nil)
+		return
+	}
 
-  respondWithJson(w, http.StatusOK, returnVal{
-    Valid: true,
-  })
+	respondWithJson(w, http.StatusOK, returnVal{
+		CleanedBody: removeProfainity(params.Body),
+	})
 
 }
